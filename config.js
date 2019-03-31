005020356dd71f6e6295c21bf4fe50a823ca3795
Gaozizhong <631923319@qq.com>
2019-03-31 21:34:27 +0800

    纯商业级小程序开发
    为毕业设计的小程序端开发积攒经验呢

---
+++ config.js
@@ -0,0 +1,9 @@
+
+const config = {
+  api_douban: "https://douban.uieee.com",
+  api_base_url: "https://douban.uieee.com",
+  api_test_url: "http://localhost:81",
+  appkey: ""
+}
+
+export { config }
