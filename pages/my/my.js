2fa3924341ce810c6b62bb608afced5ba15ee20b
Gaozizhong <631923319@qq.com>
2019-04-03 10:25:44 +0800

    完成学习，还有许多细节没有实现

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,66 +1,145 @@
 // pages/my/my.js
+import {
+    ClassicModel
+} from '../../models/classic.js'
+
+import{
+    BookModel
+}from '../../models/book.js'
+
+const classicModel = new ClassicModel()
+const bookModel = new BookModel()
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        authorized:false,
+        userInfo:null,
+        bookCount:0,
+        classics:null,
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        this.userAuthorized()
+        this.getMyBookCount()
+        this.getMyFavor()
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    },
 
-  },
+    getMyFavor(){
+        classicModel.getMyFavor(res=>{
+            this.setData({
+                classics:res
+            })
+        })
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    getMyBookCount(){
+        bookModel.getMyBookCount().then(res=>{
+            this.setData({
+                bookCount:res.count
+            })
+        })
+    },
 
-  },
+    userAuthorized() {
+        wx.getSetting({
+            success: data => {
+                if (data.authSetting['scope.userInfo']) {
+                    wx.getUserInfo({
+                        success: data => {
+                            this.setData({
+                                authorized:true,
+                                userInfo:data.userInfo
+                            })
+                        }
+                    })
+                }
+            }
+        })
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    onGetUserInfo(event) {
+        const userInfo = event.detail.userInfo
+        if(userInfo){
+            this.setData({
+                userInfo,
+                authorized: true
+            })
+        }
+        
+    },
 
-  },
+    onJumpToAbout(event){
+        wx.redirectTo({
+            url: '/pages/about/about',
+            success: function(res) {},
+            fail: function(res) {},
+            complete: function(res) {},
+        })
+    },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    onStudy(event){
+        wx.redirectTo({
+            url: '/pages/course/course',
+            success: function (res) { },
+            fail: function (res) { },
+            complete: function (res) { },
+        })
+    },  
 
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+       
+    },
+    
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    },
 
-  },
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    },
 
-  }
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    }
 })
